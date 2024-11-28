1f0961f841c149cbcdad371bbf7dff3dda3e25f2
Ahmed Moustafa <abc94895@gmail.com>
2024-11-28 09:43:47 +0000

    punto 2, 3, 4

--- Moustafa/src/app/app.component.ts
+++ Moustafa/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SecondComponent, ThirdComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
